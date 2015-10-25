05b938fb76cbc709fad9e37cee040ce42d62c658
Alexander von Below <Alex@vonBelow.Com>
2015-10-25 16:47:20 +0100

    Initial Commit

---
+++ Lost In Translation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lost In Translation
+//
+//  Created by Alexander v. Below on 25.10.15.
+//  Copyright © 2015 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lost In TranslationTests/Lost_In_TranslationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lost_In_TranslationTests.swift
+//  Lost In TranslationTests
+//
+//  Created by Alexander v. Below on 25.10.15.
+//  Copyright © 2015 Alexander von Below. All rights reserved.
+//
+
+import XCTest
+@testable import Lost_In_Translation
+
+class Lost_In_TranslationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
