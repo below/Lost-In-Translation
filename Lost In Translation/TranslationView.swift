4aeca588334963829d0a37a12645514b4c9c2ad3
Alexander von Below <alex@vonbelow.com>
2024-03-04 22:52:02 +0100

    Update Deployment Target to iOS 13 for SwiftUI

---
+++ Lost In Translation/TranslationView.swift
@@ -0,0 +1,19 @@
+//
+//  TranslationView.swift
+//  Lost In Translation
+//
+//  Created by Alexander von Below on 04.03.24.
+//  Copyright © 2024 Alexander von Below. All rights reserved.
+//
+
+import SwiftUI
+
+struct TranslationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslationView()
+}
